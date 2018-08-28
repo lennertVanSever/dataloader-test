8e20c4f737ed9633f83fc47c8028e0a4f825b52c
Lennert <lennert_vansever@hotmail.be>
2018-08-28 10:37:03 +0200

    enablin introspection

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
 
 import { resolvers } from './data/resolvers';
 import { typeDefs } from './data/Schema';
 import loaders from './data/loaders';
-
+import cors from 'cors';
 
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    loaders //Check why this is necessary
-  },
+  dataSources: () => loaders,
   engine: {
     apiKey: process.env.API_KEY_APOLLO_ENGINE
   },
-  playground: {
-    settings: {
-      'editor.theme': 'light',
-      'editor.cursorShape': 'line'
-    },
-  }
+  introspection: true,
+  playground: true
 });
+
+/*
+playground: {
+  settings: {
+    'editor.theme': 'light',
+    'editor.cursorShape': 'line'
+  },
+}*/
 
 const port = process.env.PORT || 4000;
 server.listen({ port }).then(({ url }) => {
